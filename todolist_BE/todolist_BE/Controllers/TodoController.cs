880fe2cf96e82c7ec7fa4daba6a25b17974a8242
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-10-25 16:14:56 +0700

    add method put post and delete for app

--- todolist_BE/todolist_BE/Controllers/TodoController.cs
+++ todolist_BE/todolist_BE/Controllers/TodoController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
 using todolist_BE.Data;
 using todolist_BE.Data.Entities;
 
@@ -18,11 +20,63 @@ namespace todolist_BE.Controllers
 
 
         [HttpGet]
-        public IEnumerable<Todo> Get()
+        public async Task<ActionResult<IEnumerable<Todo>>> GetToDo()
         {
-            return  _todo.Todos;
+            return await _todo.Todos.ToListAsync();
         }
 
+
+
+
+        [HttpGet("{id}")]
+        
+
+        public async Task<ActionResult<Todo>> GetTodobyID(string id)
+        {
+            var tmp = await _todo.Todos.FindAsync(id);
+            if(tmp == null)
+            {
+                return NotFound();
+            }
+            return tmp;
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<Todo>> AddTodo(Todo todo)
+        {
+           _todo.Todos.Add(todo);
+            await _todo.SaveChangesAsync();
+            return CreatedAtAction("GetTodobyID", new { Id = todo.Id }, todo);
+
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Todo>> DeleteTodo(string id)
+        {
+            var tmp = await _todo.Todos.FindAsync(id);
+            if (tmp == null)
+            {
+                return BadRequest();
+            }
+            _todo.Todos.Remove(tmp);
+            await _todo.SaveChangesAsync();
+            return tmp;
+        }
+
+
+        [HttpDelete]
+        public async Task<ActionResult<Todo>> DeleteAll()
+        {
+            foreach (var item in _todo.Todos)
+            {
+                _todo.Todos.Remove(item);
+            }
+
+            await _todo.SaveChangesAsync();
+            return Ok();
+        }
 
     }
 }
